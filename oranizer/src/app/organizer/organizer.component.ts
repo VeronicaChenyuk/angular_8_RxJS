e6ef5188fe37576ff2f76db8338ae68bb6df611d
VeronicaChenyuk <beresneva.veronika@mail.ru>
2020-10-05 12:10:46 +0300

    53-41

--- oranizer/src/app/organizer/organizer.component.ts
+++ oranizer/src/app/organizer/organizer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateService } from '../shared/data.service';
 
 @Component({
   selector: 'app-organizer',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dateService: DateService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
--- oranizer/src/app/shared/moment.pipe.ts
+++ oranizer/src/app/shared/moment.pipe.ts
@@ -6,7 +6,7 @@ import * as moment from 'moment';
     pure: false
 })
 export class MomentPipe implements PipeTransform {
-    transform(m: moment.Moment, format: string = 'MMMM YYYY'): any {
+    transform(m: moment.Moment, format: string = 'MMMM YYYY'): string {
         return m.format(format)
 
     }
